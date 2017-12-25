28c45b38f337e5a251af77330f07396587eb441a
cheney zhang <cheney@cheneydeMacBook-Pro.local>
2017-12-25 23:09:17 +0800

    update bug and author and email

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,7 +2,7 @@
   <div id="editor">
     <nav>
       <ol>
-        <li v-for="i in items" v-bind:class="{active: currentTab === i}" v-on:click="currentTab = i">
+        <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active: currentTab === i}" v-on:click="currentTab = i">
           <svg class="icon" aria-hidden="true">
             <use v-bind:xlink:href="`#icon-${icons[i]}`"></use>
           </svg>
@@ -10,12 +10,9 @@
       </ol>
     </nav>
     <ol class="panels">
-      <li v-bind:class="{active: currentTab === 0}">1</li>
-      <li v-bind:class="{active: currentTab === 1}">2</li>
-      <li v-bind:class="{active: currentTab === 2}">3</li>
-      <li v-bind:class="{active: currentTab === 3}">4</li>
-      <li v-bind:class="{active: currentTab === 4}">5</li>
-      <li v-bind:class="{active: currentTab === 5}">6</li>
+      <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active: currentTab === i}" >
+        tab{{i+1}}
+      </li>
     </ol>
   </div>
 </template>
@@ -25,7 +22,6 @@
     data() {
       return {
         currentTab: 0,
-        items: [0,1,2,3,4,5],
         icons: ['shenfenxinxi','gongzuo','xueli','projectunpressed','award4','phone1']
       }
     }
