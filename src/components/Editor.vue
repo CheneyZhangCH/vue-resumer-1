49fcd23855b4f2ea198dd0984f1aa5f51e6f6769
cheney zhang <cheney_zhang@foxmail.com>
2017-12-25 23:18:10 +0800

    update committer and email

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,7 +2,7 @@
   <div id="editor">
     <nav>
       <ol>
-        <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active: currentTab === i}" v-on:click="currentTab = i">
+        <li v-for="i in items" v-bind:class="{active: currentTab === i}" v-on:click="currentTab = i">
           <svg class="icon" aria-hidden="true">
             <use v-bind:xlink:href="`#icon-${icons[i]}`"></use>
           </svg>
@@ -10,7 +10,7 @@
       </ol>
     </nav>
     <ol class="panels">
-      <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active: currentTab === i}" >
+      <li v-for="i in items" v-bind:class="{active: currentTab === i}" >
         tab{{i+1}}
       </li>
     </ol>
@@ -22,6 +22,7 @@
     data() {
       return {
         currentTab: 0,
+        items: [0,1,2,3,4,5],
         icons: ['shenfenxinxi','gongzuo','xueli','projectunpressed','award4','phone1']
       }
     }
