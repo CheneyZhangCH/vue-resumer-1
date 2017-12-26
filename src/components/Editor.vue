2a7c353d3c3708771823bc9a2cb19052227f85e1
cheney zhang <cheney_zhang@foxmail.com>
2017-12-26 23:35:45 +0800

    完善简历编辑区块

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -10,8 +10,53 @@
       </ol>
     </nav>
     <ol class="panels">
-      <li v-for="i in items" v-bind:class="{active: currentTab === i}" >
-        tab{{i+1}}
+      <li v-bind:class="{active: currentTab === 0}">
+        <h2>个人信息</h2>
+        <el-form v-model="profile">
+          <el-form-item label="姓名">
+            <el-input v-model="profile.name"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-input v-model="profile.sex"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="profile.age"></el-input>
+          </el-form-item>
+          <el-form-item label="所在地">
+            <el-input v-model="profile.city"></el-input>
+          </el-form-item>
+        </el-form>
+      </li>
+      <li v-bind:class="{active: currentTab === 1}">
+        <h2>工作经历</h2>
+        <el-form v-model="workExperience" v-for="(work, index) in workExperience" >
+          <el-form-item label="公司">
+            <el-input v-model="workExperience.name"></el-input>
+          </el-form-item>
+          <el-form-item label="时间">
+            <el-input v-model="workExperience.period"></el-input>
+          </el-form-item>
+          <el-form-item label="工作内容">
+            <el-input v-model="profile.content"></el-input>
+          </el-form-item>
+          <el-button type="primary" icon="el-icon-delete" v-on:click="removeWorkExperience(index)"></el-button>
+
+
+        </el-form>
+        <el-button type="primary" icon="el-icon-edit" v-on:click="addWorkExperience"></el-button>
+
+      </li>
+      <li v-bind:class="{active: currentTab === 2}">
+        <h2>教育经历</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 3}">
+        <h2>项目经历</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 4}">
+        <h2>获奖情况</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 5}">
+        <h2>联系方式</h2>
       </li>
     </ol>
   </div>
@@ -22,8 +67,25 @@
     data() {
       return {
         currentTab: 0,
-        items: [0,1,2,3,4,5],
-        icons: ['shenfenxinxi','gongzuo','xueli','projectunpressed','award4','phone1']
+        items: [0, 1, 2, 3, 4, 5],
+        icons: ['shenfenxinxi', 'gongzuo', 'xueli', 'projectunpressed', 'award4', 'phone1'],
+        profile: {
+          name: "",
+          sex: '',
+          age: '',
+          city: ''
+        },
+        workExperience: [
+          {name:'', period: '', content:''}
+        ]
+      }
+    },
+    methods: {
+      addWorkExperience (){
+        this.workExperience.push({name:'', period: '', content:''})
+      },
+      removeWorkExperience (index){
+        this.workExperience.splice(index, 1)
       }
     }
   }
