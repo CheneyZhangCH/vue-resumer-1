206a7c204a47eb17fbd5de8811bc7b31e04e55a5
cheney zhang <cheney_zhang@foxmail.com>
2018-01-16 23:38:35 +0800

    添加注册UI

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -5,15 +5,66 @@
         R
       </div>
       <div class="actions">
-        <el-button plain>登陆</el-button>
-        <el-button plain>注册</el-button>
+        <el-button plain v-on:click="login()">登陆</el-button>
+        <el-button plain v-on:click="login()">注册</el-button>
+        <!--<login v-show="toLogin === true"></login>-->
+        <div class="login-wrapper" v-show="toLogin === true">
+          <!--<form class="ligin-form">-->
+          <!--<input type="text" name="username" placeholder="请输入用户名">-->
+          <!--<input type="password" name="password" placeholder="请输入密码">-->
+          <!--<p> 温馨提示：未注册用户，登录时将自动注册。</p>-->
+          <!--<input type="submit" value="登录">-->
+          <!--<span v-on:click="closeLogin()">关闭</span>-->
+          <!--</form>-->
+
+          <el-form label-width="80px">
+            <el-form-item label="用户名">
+              <el-input type="text" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input type="password" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <div>
+              温馨提示：未注册用户，登录时将自动注册。
+            </div>
+            <el-form-item>
+              <el-button type="primary">立即创建</el-button>
+              <el-button v-on:click="cansel()">取消</el-button>
+            </el-form-item>
+
+          </el-form>
+
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+
+  //  import login from './login.vue'
+
+  export default {
+    data() {
+      return {
+        toLogin: false
+      }
+    },
+
+    components: {
+//      login: login,
+    },
+    methods: {
+      login() {
+        this.toLogin = true
+      },
+      cansel() {
+        this.toLogin = false
+      }
+    }
+
+  }
 </script>
 
 <style scoped lang="scss">
@@ -23,8 +74,8 @@
     .content {
       height: 80px;
       max-width: 1280px;
-      margin-left:auto;
-      margin-right:auto;
+      margin-left: auto;
+      margin-right: auto;
       display: flex;
       justify-content: space-between;
       align-items: center;
@@ -42,6 +93,17 @@
         color: white;
       }
       .actions {
+        .login-wrapper {
+          background: hsla(0, 0%, 0%, 0.1);
+          position: fixed;
+          width: 100%;
+          height: 100%;
+          left: 0;
+          top: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
       }
     }
   }
