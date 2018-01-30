52f93816e1e4776d8837dcf8ec1ca8abc3ed1013
cheney zhang <cheney_zhang@foxmail.com>
2018-01-30 22:36:10 +0800

    build for producton

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-resumer-1/dist/',
 
     /**
      * Source Maps
